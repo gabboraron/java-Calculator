a579002078600253ed14eeaa8514890b0a44d9f8
gabboraron <gabboraron@gmail.com>
2017-10-08 01:48:19 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+class Calculator {
+    public static void main(String[] args) {
+        /*
+		//test
+        System.out.println("a megadott ertekek: ");
+        System.out.println(args[0]);
+        System.out.println("//");
+        System.out.println(args[1]);
+        System.out.println("//");
+        System.out.println(args[2]);
+        System.out.println("//");
+		*/
+		
+		import java.util.regex.Matcher;
+		import java.util.regex.Pattern;
+		
+		String line =args[0];
+		String pattern = ",";
+		
+		
+    }
+}
